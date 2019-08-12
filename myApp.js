94da1a414fed7ae6b9a52a34e8677f5f58c24ee6
Harry <harry@desmart.com>
2019-08-12 21:52:55 +0200

    saving data to db

--- myApp.js
+++ myApp.js
@@ -6,116 +6,45 @@
 
 import express from 'express'
 import { has } from 'ramda'
-var bodyParser = require('body-parser')
+import mongoose from 'mongoose'
+const bodyParser = require('body-parser')
 
-const app = express();
+const nameSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String
+})
+const User = mongoose.model("User", nameSchema)
 
-// --> 7)  Mount the Logger middleware here
+mongoose.connect('mongodb+srv://harry:123Qwerty@cluster0-oxvpy.mongodb.net/test?retryWrites=true&w=majority')
+
+const app = express()
+
 const logger = (req, res, next) => {
     if(has('token', req.headers)){
         console.log(req.ip)
-        console.log('has token')
+        console.log('TOKEN TOKEN TOKEN TOKEN !!')
     }
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
 }
 app.use(logger)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// --> 11)  Mount the body-parser middleware  here
-const parseBody = bodyParser.urlencoded({extended: false})
-app.use(parseBody)
-
-/** 1) Meet the node console. */
-console.log("Hello World");
-
-/** 2) A first working Express Server */
-// const handler = function(req, res) {
-// // res.send('Hello Express');
-// }
-// app.get('/', handler)
-
-/** 3) Serve an HTML file */
 const serveHtml = function(req, res) {
     res.sendFile(`${__dirname}/views/index.html`)
 }
 app.get('/', serveHtml)
 
-/** 4) Serve static assets  */
-const assets = express.static(`${__dirname}/public`)
-app.use(assets)
-
-/** 5) serve JSON on a specific route */
-const response = {"message": "Hello json"}
-// const serveJson = function(req, res) {
-// res.json(response)
-// }
-// app.get('/json', serveJson)
-
-/** 6) Use the .env file to configure the app */
-const serveJson = function(req, res) {
-    res.json(
-        process.env.MESSAGE_STYLE === 'uppercase' ?
-            {
-                ...response,
-                message: response.message.toUpperCase()
-            }
-            : response)
-}
-app.get('/json', serveJson)
-
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
-
-
-/** 8) Chaining middleware. A Time server */
-app.get('/now', function(req, res, next) {
-    req.time = new Date().toString()
-    next();
-}, function(req, res) {
-    res.json({time: req.time});
-})
-
-/** 9)  Get input from client - Route parameters */
-app.get('/:word/echo', function(req, res, next){
-    next()
-}, function(req, res, next){
-    res.json({
-        echo: req.params.word
-    })
-})
-
-/** 10) Get input from client - Query parameters */
-// /name?first=<firstname>&last=<lastname>
-// app.get('/name', function(req, res, next){
-//   console.log(req.query)
-//   next()
-// }, function(req, res, next){
-//   const params = req.query
-//   res.json({
-//     name: `${params.first} ${params.last}`
-//   })
-// })
-
 app.route('/user')
-    .get((req, res) => res.json({
-        user: `${req.query.first} ${req.query.last}`
-    }))
-    .post((req, res) => res.json({
-        user: `${req.body.first} ${req.body.last}`
-    }))
-
-/** 11) Get ready for POST Requests - the `body-parser` */
-// place it before all the routes !
-
-
-/** 12) Get data form POST  */
-
-
-
-// This would be part of the basic setup of an Express app
-// but to allow FCC to run tests, the server is already active
-/** app.listen(process.env.PORT || 3000 ); */
-
-//---------- DO NOT EDIT BELOW THIS LINE --------------------
-
-module.exports = app;
+    .post((req, res) => {
+        const myData = new User(req.body)
+        myData.save()
+            .then(item => {
+                res.send("item saved to database");
+            })
+            .catch(err => {
+                res.status(400).send("unable to save to database");
+            })
+    })
+module.exports = app
