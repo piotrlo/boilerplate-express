8986aaec93d23a8567f45c66276b4a16c7e12ed3
Harry <harry@desmart.com>
2019-08-12 20:43:15 +0200

    installed mongodb

--- myApp.js
+++ myApp.js
@@ -1,21 +1,29 @@
+// • Serwer ma każdy request sprawdzać czy header token istnieje i ma wartość admin i jeśli tak to logować ip requesta do konsoli
+// • Serwer ma mieć 4 routy o nazwie /user na matody: POST, GET, DELETE, PUT
+// • Serwer ma supportować cors
+// • Każdy request który przyjdzie do backu musi być zapisany do bazy
+// • Każdy request ma zawracać ilość zapisanych requestów w bazie i ID ostatniego stowrzonego
 
-var express = require('express');
-var app = express();
+import express from 'express'
+import { has } from 'ramda'
+var bodyParser = require('body-parser')
 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
-const logger = (req, res, next) => {
-console.log(`${req.method} ${req.path} - ${req.ip}`);
-next();
-}
-
-app.use(logger)
+const app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+const logger = (req, res, next) => {
+    if(has('token', req.headers)){
+        console.log(req.ip)
+        console.log('has token')
+    }
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+}
+app.use(logger)
 
 // --> 11)  Mount the body-parser middleware  here
-
+const parseBody = bodyParser.urlencoded({extended: false})
+app.use(parseBody)
 
 /** 1) Meet the node console. */
 console.log("Hello World");
@@ -28,7 +36,7 @@ console.log("Hello World");
 
 /** 3) Serve an HTML file */
 const serveHtml = function(req, res) {
-res.sendFile(`${__dirname}/views/index.html`)
+    res.sendFile(`${__dirname}/views/index.html`)
 }
 app.get('/', serveHtml)
 
@@ -45,26 +53,57 @@ const response = {"message": "Hello json"}
 
 /** 6) Use the .env file to configure the app */
 const serveJson = function(req, res) {
-res.json(
-  process.env.MESSAGE_STYLE === 'uppercase' ?
-  {
-    ...response,
-    message: response.message.toUpperCase()
-  } 
-  : response)
+    res.json(
+        process.env.MESSAGE_STYLE === 'uppercase' ?
+            {
+                ...response,
+                message: response.message.toUpperCase()
+            }
+            : response)
 }
 app.get('/json', serveJson)
 
-/** 8) Chaining middleware. A Time server */
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
 
+
+/** 8) Chaining middleware. A Time server */
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString()
+    next();
+}, function(req, res) {
+    res.json({time: req.time});
+})
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', function(req, res, next){
+    next()
+}, function(req, res, next){
+    res.json({
+        echo: req.params.word
+    })
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
+// app.get('/name', function(req, res, next){
+//   console.log(req.query)
+//   next()
+// }, function(req, res, next){
+//   const params = req.query
+//   res.json({
+//     name: `${params.first} ${params.last}`
+//   })
+// })
 
-  
+app.route('/user')
+    .get((req, res) => res.json({
+        user: `${req.query.first} ${req.query.last}`
+    }))
+    .post((req, res) => res.json({
+        user: `${req.body.first} ${req.body.last}`
+    }))
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
@@ -79,4 +118,4 @@ app.get('/json', serveJson)
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
